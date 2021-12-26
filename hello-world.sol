726c4da2906eca3094b5815c9d1814d739600d7e
Gesiel Lucas Ferreira <32823495+gesiellucas@users.noreply.github.com>
2021-12-26 12:32:27 -0300

    Initialize sol

---
+++ hello-world.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.22;
+
+contract HelloEarth {
+    function renderHelloEarth () public pure returns (string) {
+        return 'Morning, Earth!';
+    }
+}
